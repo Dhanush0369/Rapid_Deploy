8bb9a066e811ddb7becc78162b555436619da813
Dhanush0369 <dhanush789245@gmail.com>
2024-08-19 23:01:21 +0530

    Added .env file
    Signed-off-by: Dhanush0369 <dhanush789245@gmail.com>

--- request-service/src/index.ts
+++ request-service/src/index.ts
@@ -1,12 +1,17 @@
 import express from "express";
-import {S3} from "aws-sdk";
+import { S3 } from 'aws-sdk';
+import dotenv from 'dotenv';
 
 const app = express();
+
+
+dotenv.config();
+
 const s3 = new S3({
-    accessKeyId: "28cc04cc25ebf7a76e0000811f95e8b4",
-    secretAccessKey: "e91032aa213842333833351700bba1c30e490af87605dc97af78d7e889415929",
-    endpoint: "https://4d1ec783bad1c616696abe541ec3e06c.r2.cloudflarestorage.com"
-})
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    endpoint: process.env.AWS_ENDPOINT
+});
 
 
 app.get("/*", async (req,res) => {
