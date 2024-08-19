ccfba9b2a7a8778540553830e5f27cd0cefd9189
Dhanush0369 <dhanush789245@gmail.com>
2024-08-19 23:04:02 +0530

    Updated .gitignore file
    Signed-off-by: Dhanush0369 <dhanush789245@gmail.com>

--- deploy-service/src/aws.ts
+++ deploy-service/src/aws.ts
@@ -1,12 +1,15 @@
-import {S3} from "aws-sdk";
 import fs from "fs";
 import path from "path";
+import { S3 } from 'aws-sdk';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const s3 = new S3({
-    accessKeyId: "28cc04cc25ebf7a76e0000811f95e8b4",
-    secretAccessKey: "e91032aa213842333833351700bba1c30e490af87605dc97af78d7e889415929",
-    endpoint: "https://4d1ec783bad1c616696abe541ec3e06c.r2.cloudflarestorage.com"
-})
+    accessKeyId: process.env.CF_ACCESS_KEY_ID,
+    secretAccessKey: process.env.CF_SECRET_ACCESS_KEY,
+    endpoint: process.env.CF_ENDPOINT
+});
 
 export async function downloadS3Files(prefix:string){
     const allFiles = await s3.listObjectsV2({
